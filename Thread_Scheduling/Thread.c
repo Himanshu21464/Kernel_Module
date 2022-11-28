3915c8a350d0f0cda3f462ba2c395fc851dc740a
Himanshu <98085386+Himanshu21464@users.noreply.github.com>
2022-11-28 12:46:55 +0530

    Update Thread.c

--- Thread_Scheduling/Thread.c
+++ Thread_Scheduling/Thread.c
@@ -27,11 +27,9 @@ int main(){
   struct sched_param param;
   
   FILE * filename;
-  filename = fopen("Thread_Scheduling_Data.txt","w");
+  filename = fopen("Thread_Scheduling_Data.txt","a");
   S1=clock();
   pthread_create(&THREAD_1, NULL, countA, NULL);
-  pthread_create(&THREAD_1, NULL, countB, NULL);
-  pthread_create(&THREAD_1, NULL, countC, NULL);
   pthread_setschedparam(THREAD_1, SCHED_OTHER, &param);
   pthread_join(THREAD_1, NULL);
   
@@ -40,20 +38,18 @@ int main(){
   printf("Time to run THREAD_1: %f\n",F1/3000000);
   fprintf(filename," %f\n",F1/3000000);
   for(int prio_num = 1; prio_num<=4; prio_num++){
-    printf("For priority: %d",prio_num);
+    //printf("For priority: %d",prio_num);
   
     S2=clock();
     pthread_setschedprio(THREAD_1,prio_num);
     pthread_join(THREAD_1, NULL);
     E2=clock();
     F2=(double)(E2-S2);
-    printf("Time to run THREAD_2: %f\n",F2/1000000);
+    //printf("Time to run THREAD_2: %f\n",F2/1000000);
     fprintf(filename," %f\n",F2/1000000);
   }
   S3=clock();
-  pthread_create(&THREAD_2, NULL, countA, NULL);
   pthread_create(&THREAD_2, NULL, countB, NULL);
-  pthread_create(&THREAD_2, NULL, countC, NULL);
   pthread_setschedparam(THREAD_2, SCHED_RR, &param);
   pthread_join(THREAD_2, NULL);
   E3=clock();
@@ -62,18 +58,16 @@ int main(){
   fprintf(filename,"%f\n",F3/3000000);
 
   for(int prio_num = 1; prio_num<=4; prio_num++){
-    printf("For priority: %d",prio_num);
+    //printf("For priority: %d",prio_num);
     S4=clock();
     pthread_setschedprio(THREAD_2,prio_num);
     pthread_join(THREAD_2, NULL);
     E4=clock();
     F4=(double)(E4-S4);
-    printf("Time to run THREAD_2: %f\n",F4/1000000);
+    //printf("Time to run THREAD_2: %f\n",F4/1000000);
     fprintf(filename,"%f\n",F4/1000000);  
   }
   S5=clock();
-  pthread_create(&THREAD_3, NULL, countA, NULL);
-  pthread_create(&THREAD_3, NULL, countB, NULL);
   pthread_create(&THREAD_3, NULL, countC, NULL);
   pthread_setschedparam(THREAD_3, SCHED_FIFO, &param);
   pthread_join(THREAD_3, NULL);
@@ -83,13 +77,13 @@ int main(){
   fprintf(filename," %f\n",F5/3000000);
 
   for(int prio_num = 1; prio_num<=4; prio_num++){
-    printf("For priority: %d",prio_num);
+    //printf("For priority: %d",prio_num);
     S6=clock();
     pthread_setschedprio(THREAD_3,prio_num);
     pthread_join(THREAD_3, NULL);
     E6=clock();
     F6=(double)(E6-S6);
-    printf("Time to run THREAD_3: %f\n",F6/1000000);
+    //printf("Time to run THREAD_3: %f\n",F6/1000000);
     fprintf(filename,"%f\n",F6/1000000); 
   }
   fclose(filename);
